0913efc94008e11b1222747b5668ecf2810310c7
jemima <ashbeca16@gmail.com>
2023-11-13 13:28:23 +0200

    adding all files

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const integer = parseInt(process.argv[2]);
+let stringX = '';
+if (!integer) {
+  console.log('Missing size');
+}
+for (let i = 0; i < integer; i++) {
+  for (let j = 0; j < integer; j++) {
+    stringX += 'X';
+  }
+  stringX += '\n';
+}
+console.log(stringX);
